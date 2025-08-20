8617dcf51760751ea481716a164240ca60614a41
Vinicius Martin <viniciusmartindeoliveira@hotmail.com>
2025-08-20 23:28:38 +0000

    Cadastro dos Territórios

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -9,6 +9,24 @@ struct Territorio {
 
 int main() {
 
+    struct Territorio lista_de_territorios[5];
+    int i;
+
+    printf("--- Cadastro de 5 Territorios ---\n\n");
+    for (i = 0; i < 5; i++) {
+        printf("Territorio %d:\n", i + 1);
+
+        printf("Nome: ");
+        scanf("%s", lista_de_territorios[i].nome);
+
+        printf("Cor do exercito: ");
+        scanf("%s", lista_de_territorios[i].cor);
+
+        printf("Quantidade de tropas: ");
+        scanf("%d", &lista_de_territorios[i].tropas);
+        printf("\n");
+    }
+
 
     return 0;
 }
